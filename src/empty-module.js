42225f2ce17c3dbc93602e145d66dacb525abc48
Parokor <parallelkore@gmail.com>
2025-08-07 06:02:45 +0200

    Update empty-module.js

--- src/empty-module.js
+++ src/empty-module.js
@@ -1,6 +1,25 @@
+
 // src/empty-module.js
-export const toast = () => {};
-export const Toaster = () => null;
-export const motion = {};
-export const AnimatePresence = ({ children }) => children;
-export default { toast, Toaster, motion, AnimatePresence };
+import React from 'react'
+
+// Para react-hot-toast
+export const toast = () => {}
+export const Toaster = () => null
+
+// Para framer-motion
+export const motion = {}
+export const AnimatePresence = ({ children }) => children
+
+// Para react-icons
+export const GenIcon = () => React.createElement('span')
+export const IconBase = () => React.createElement('span')
+
+// Exports por defecto
+export default { 
+  toast, 
+  Toaster, 
+  motion, 
+  AnimatePresence,
+  GenIcon,
+  IconBase
+}
